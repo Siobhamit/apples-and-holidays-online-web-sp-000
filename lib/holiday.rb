8d28b42399de9df82e52f77937a080e98b2b76c0
Siobhan The Destroyer <siobhancreagan@gmail.com>
2021-05-21 04:20:38 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -64,7 +64,7 @@ def all_supplies_in_holidays(holiday_hash)
   # etc.
   new_hash = holiday_hash
 new_hash.each do |season_hash, data_hsh|
-  season_hash.to_s.split.capitalize.join
+  season_hash.join.to_s.split.capitalize!.join
     puts "#{season_hash}:"
   data.each do |holiday_name_hash, data_hsh|
     supplies = data_hsh.join(", ")
