f49b6c61869d9761e50479fe4ac78e80fc3e1343
Siobhan The Destroyer <siobhancreagan@gmail.com>
2021-05-21 04:15:33 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -17,20 +17,22 @@ def second_supply_for_fourth_of_july(holiday_hash)
   #     :memorial_day => ["BBQ"]
   #   }
   # }
-  # return the second element in the 4th of July array
+return holiday_hash[:summer][:fourth_of_july][1]
 end
 
 def add_supply_to_winter_holidays(holiday_hash, supply)
   # holiday_hash is identical to the one above
   # add the second argument, which is a supply, to BOTH the
   # Christmas AND the New Year's arrays
-
+holiday_hash[:winter].each do |holiday, supplies|
+  supplies << supply
 end
-
+end
 
 def add_supply_to_memorial_day(holiday_hash, supply)
   # again, holiday_hash is the same as the ones above
   # add the second argument to the memorial day array
+  holiday_hash[:spring][:memorial_day] << supply
 
 end
 
@@ -38,11 +40,18 @@ def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_arr
   # code here
   # remember to return the updated hash
 
+  holiday_hash[season.to_sym].merge!(holiday_name.to_sym => supply_array.to_a)
+return holiday_hash
 end
 
 def all_winter_holiday_supplies(holiday_hash)
   # return an array of all of the supplies that are used in the winter season
-
+supplies = []
+holiday_hash[:winter].each do |holiday, supply_array|
+  supplies.concat(supply_array)
+end
+  supplies.flatten!
+  return supplies
 end
 
 def all_supplies_in_holidays(holiday_hash)
@@ -53,7 +62,15 @@ def all_supplies_in_holidays(holiday_hash)
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
-
+  new_hash = holiday_hash
+new_hash.each do |season_hash, data_hsh|
+  season_hash.to_s.capitalize
+    puts "#{season_hash}:"
+  data.each do |holiday_name_hash, data_hsh|
+    supplies = data_hsh.join(", ")
+    puts "#{holiday_name_hash}: #{supplies}"
+  end
+end
 end
 
 def all_holidays_with_bbq(holiday_hash)
@@ -61,10 +78,3 @@ def all_holidays_with_bbq(holiday_hash)
   # include the string "BBQ"
 
 end
-
-
-
-
-
-
-
